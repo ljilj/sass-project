fd05c6f46542d3d6bd56d633235a644452ea1a2b
ljilj <ljilja.antonic91@gmail.com>
2020-12-27 17:06:45 +0100

    Completed navbar and page-loader

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
-const navToggle = document.querySelector('.nav-toggle');
-const linksContainer = document.querySelector('.nav__container');
+// Toggle menu
+const navToggle = document.querySelector('.js-menu-toggle');
 const links = document.querySelector('.links__container');
 
-navToggle.addEventListener('click', function(){
-    const containerHeight = linksContainer.getBoundingClientRect().height;
-    // console.log(containerHeight);
-    const linksHeight = links.getBoundingClientRect().height;
-    // console.log(linksHeight);
-
-    if(containerHeight === 0) {
-        linksContainer.style.height = `${linksHeight}px`;
-    } else {
-        linksContainer.style.height = 0;
-    }
+navToggle.addEventListener('click', function() {
+    links.classList.toggle("active-menu");
 });
+
+
+// Page-loader
+$(document).ready(function() {
+	
+	setTimeout(function(){
+		$('body').addClass('loaded');
+		$('h1').css('color','#000000');
+	}, 1000);
+	
+});
+
+
